e84d3cff3ad99435b95513a119e6732ffe9000b1
viniciuspiotto <viniciuspiotto.dev@gmail.com>
2025-02-02 11:04:06 -0300

    chore: config tailwindcss to v3 & add shadcnUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 function App() {
   return (
     <div>
-      <h1 className="text-4xl text-red-300">hello world!</h1>
+      <h1 className="text-4xl text-red-500">hello world!</h1>
+      <Button>Click me!</Button>
     </div>
   );
 }
