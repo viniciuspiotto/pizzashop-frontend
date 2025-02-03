631f0e3de311f6cc448dc34a5b621a2914c317cf
viniciuspiotto <viniciuspiotto.dev@gmail.com>
2025-02-03 19:29:52 -0300

    feat(profile): implement modified profile managed

---
+++ src/api/updateProfile.ts
@@ -0,0 +1,10 @@
+import { api } from "@/lib/axios";
+
+interface updateProfileBody {
+  name: string;
+  description: string | null;
+}
+
+export async function updateProfile({ name, description }: updateProfileBody) {
+  await api.put("/profile", { name, description });
+}
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -13,6 +13,8 @@ const buttonVariants = cva(
           "bg-primary text-primary-foreground shadow hover:bg-primary/90",
         destructive:
           "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
+        success:
+          "bg-emerald-500 text-white shadow-sm hover:bg-emerald-500/90 dark:bg-emerald-600 dark:hover:bg-emerald-600/90",
         outline:
           "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
         secondary:
