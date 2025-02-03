0813d76666f91fafd2d6831dbde460a02f1dd432
viniciuspiotto <viniciuspiotto.dev@gmail.com>
2025-02-03 09:53:08 -0300

    feat(dashboard): implement revenue chart

--- src/pages/app/dashboard/dashboard.tsx
+++ src/pages/app/dashboard/dashboard.tsx
@@ -2,16 +2,20 @@ import DayOrdersAmountCard from "./dayOrdersAmountCard";
 import MonthCanceledOrdersAmountCard from "./monthCanceledOrdersAmountCard";
 import MonthOrdersAmountCard from "./monthOrdersAmountCard";
 import MonthRevenueCard from "./monthRevenueCard";
+import RevenueChart from "./revenueChart";
 
 function Dashboard() {
   return (
     <div className="flex flex-col gap-4">
       <h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>
-      <div className="grid grid-cols-4 gap-4">
+      <div className="row-span-2 grid grid-cols-4 gap-4">
         <MonthRevenueCard />
         <MonthOrdersAmountCard />
         <DayOrdersAmountCard />
         <MonthCanceledOrdersAmountCard />
+      </div>
+      <div className="grid grid-cols-8 gap-4">
+        <RevenueChart />
       </div>
     </div>
   );
---
+++ src/pages/app/dashboard/revenueChart.tsx
@@ -0,0 +1,89 @@
+import { CartesianGrid, Line, LineChart, XAxis } from "recharts";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+
+const chartData = [
+  { date: "10/12", revenue: 1200 },
+  { date: "11/12", revenue: 750 },
+  { date: "12/12", revenue: 400 },
+  { date: "13/12", revenue: 820 },
+  { date: "14/12", revenue: 710 },
+  { date: "15/12", revenue: 300 },
+  { date: "16/12", revenue: 650 },
+];
+
+const chartConfig = {
+  revenue: {
+    label: "Receita",
+    color: "hsl(var(--chart-1))",
+  },
+} satisfies ChartConfig;
+
+function RevenueChart() {
+  return (
+    <Card className="col-span-4">
+      <CardHeader>
+        <CardTitle className="text-base font-medium">
+          Receita no periodo de 7 dias
+        </CardTitle>
+        <CardDescription>Receita diaria do periodo</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <ChartContainer config={chartConfig}>
+          <LineChart
+            accessibilityLayer
+            data={chartData}
+            margin={{
+              left: 20,
+              right: 20,
+              top: 12,
+              bottom: 12,
+            }}
+          >
+            <CartesianGrid
+              vertical={false}
+              stroke="var(--muted-foreground)"
+              className="stroke-muted-foreground/40"
+            />
+            <XAxis
+              dataKey="date"
+              tickLine={false}
+              axisLine={false}
+              tickMargin={8}
+            />
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent indicator="line" />}
+            />
+            <Line
+              dataKey={"revenue"}
+              type={"monotone"}
+              stroke="var(--color-revenue)"
+              strokeWidth={1}
+              dot={{
+                fill: "var(--color-revenue)",
+              }}
+              activeDot={{
+                r: 6,
+              }}
+            />
+          </LineChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default RevenueChart;
