98e91772d7a6c18fd01a1632892ff7f94d2abd03
viniciuspiotto <viniciuspiotto.dev@gmail.com>
2025-02-04 09:19:15 -0300

    feat(profile): add optimistic profile interface

--- src/components/storeProfileDialog.tsx
+++ src/components/storeProfileDialog.tsx
@@ -25,7 +25,7 @@ import { Textarea } from "./ui/textarea";
 
 const storeProfileSchema = z.object({
   name: z.string().min(1),
-  description: z.string(),
+  description: z.string().nullable(),
 });
 
 type StoreProfileSchema = z.infer<typeof storeProfileSchema>;
@@ -51,21 +51,37 @@ function StoreProfileDialog() {
     },
   });
 
+  function updateManagedRestaurantCache({
+    name,
+    description,
+  }: StoreProfileSchema) {
+    const cached = queryClient.getQueryData<GetManagedRestaurantResponse>([
+      "managedRestaurant",
+    ])!;
+
+    queryClient.setQueryData<GetManagedRestaurantResponse>(
+      ["managedRestaurant"],
+      {
+        ...cached,
+        name,
+        description,
+      },
+    );
+
+    return { cached };
+  }
+
   const { mutateAsync: updateProfileFn } = useMutation({
     mutationFn: updateProfile,
-    onSuccess(_, { name, description }) {
-      const cached = queryClient.getQueryData<GetManagedRestaurantResponse>([
-        "managedRestaurant",
-      ])!;
+    onMutate({ name, description }) {
+      const { cached } = updateManagedRestaurantCache({ name, description });
 
-      queryClient.setQueryData<GetManagedRestaurantResponse>(
-        ["managedRestaurant"],
-        {
-          ...cached,
-          name,
-          description,
-        },
-      );
+      return { previousProfile: cached };
+    },
+    onError(_, __, context) {
+      if (context?.previousProfile) {
+        updateManagedRestaurantCache(context.previousProfile);
+      }
     },
   });
 
